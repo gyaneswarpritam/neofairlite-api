48326a276b73de12d104066d708b500772bbddc9
gyaneswarpritam <gyaneswar.pritam@gmail.com>
2024-09-01 02:42:05 +0530

    changes

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Error handling middleware
 app.use(errorHandler);
 // Start server
-const PORT = process.env.PORT || 5002;
+const PORT = 5002;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const io = socket(server, {
     cors: {
