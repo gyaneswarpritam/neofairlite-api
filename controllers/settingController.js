4dbe02eb48f0c016e0985783db246ef607298e86
gyaneswarpritam <gyaneswar.pritam@gmail.com>
2024-09-12 23:28:42 +0530

    changes

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -5,60 +5,21 @@ const schemaValidator = require('../validators/schemaValidator');
 const slots = require('../models/slots');
 const moment = require('moment-timezone'); // Import moment-timezone
 
-// exports.createSetting = async (req, res) => {
-//     try {
-//         const validatedData = schemaValidator(settingSchema, req.body);
-//         if (validatedData.success) {
-//             const { startDateTime, endDateTime, timezone } = validatedData.data;
-
-//             // Convert startDateTime and endDateTime from the provided timezone to UTC
-//             const startUTC = moment.tz(startDateTime, timezone).utc().toDate();
-//             const endUTC = moment.tz(endDateTime, timezone).utc().toDate();
-
-//             // Update the validated data with the UTC dates
-//             validatedData.data.startDateTime = startUTC;
-//             validatedData.data.endDateTime = endUTC;
-
-//             await slots.deleteMany({});
-//             const setting = await Setting.create(validatedData.data);
-//             const successObj = successResponse('Setting Created', setting)
-//             res.status(successObj.status).send(successObj);
-//         } else {
-//             res.status(401).json({ message: validatedData.errors });
-//         }
-//     } catch (error) {
-//         res.status(400).json({ message: error.message });
-//     }
-// };
-
-// exports.updateSetting = async (req, res) => {
-//     try {
-//         const { startDateTime, endDateTime, timezone } = req.body;
-
-//         // Convert startDateTime and endDateTime from the provided timezone to UTC
-//         const startUTC = moment.tz(startDateTime, timezone).utc().toDate();
-//         const endUTC = moment.tz(endDateTime, timezone).utc().toDate();
-
-//         // Update the request body with the UTC dates
-//         req.body.startDateTime = startUTC;
-//         req.body.endDateTime = endUTC;
-
-//         const setting = await Setting.findByIdAndUpdate(req.params.id, req.body, { new: true });
-//         if (!setting) {
-//             return res.status(404).json({ message: 'Setting not found' });
-//         }
-//         await slots.deleteMany({});
-//         res.status(200).json({ data: setting, message: 'Setting updated successfully' });
-//     } catch (error) {
-//         res.status(400).json({ message: error.message });
-//     }
-// };
-
-
 exports.createSetting = async (req, res) => {
     try {
         const validatedData = schemaValidator(settingSchema, req.body);
         if (validatedData.success) {
+            const { startDateTime, endDateTime, timezone } = validatedData.data;
+
+            // Convert startDateTime and endDateTime from the provided timezone to UTC
+            const startUTC = moment.tz(startDateTime, timezone).utc().toDate();
+            const endUTC = moment.tz(endDateTime, timezone).utc().toDate();
+
+            // Update the validated data with the UTC dates
+            validatedData.data.startDateTime = startUTC;
+            validatedData.data.endDateTime = endUTC;
+
+            await slots.deleteMany({});
             const setting = await Setting.create(validatedData.data);
             const successObj = successResponse('Setting Created', setting)
             res.status(successObj.status).send(successObj);
@@ -72,22 +33,61 @@ exports.createSetting = async (req, res) => {
 
 exports.updateSetting = async (req, res) => {
     try {
-        // const validatedData = schemaValidator(settingSchema, req.body);
-        // if (validatedData.success) {
+        const { startDateTime, endDateTime, timezone } = req.body;
+
+        // Convert startDateTime and endDateTime from the provided timezone to UTC
+        const startUTC = moment.tz(startDateTime, timezone).utc().toDate();
+        const endUTC = moment.tz(endDateTime, timezone).utc().toDate();
+
+        // Update the request body with the UTC dates
+        req.body.startDateTime = startUTC;
+        req.body.endDateTime = endUTC;
+
         const setting = await Setting.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!setting) {
             return res.status(404).json({ message: 'Setting not found' });
         }
-
-        // Respond with success message
+        await slots.deleteMany({});
         res.status(200).json({ data: setting, message: 'Setting updated successfully' });
-        // } else {
-        //     res.status(401).json({ message: validatedData.errors });
-        // }
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 };
+
+
+// exports.createSetting = async (req, res) => {
+//     try {
+//         const validatedData = schemaValidator(settingSchema, req.body);
+//         if (validatedData.success) {
+//             const setting = await Setting.create(validatedData.data);
+//             const successObj = successResponse('Setting Created', setting)
+//             res.status(successObj.status).send(successObj);
+//         } else {
+//             res.status(401).json({ message: validatedData.errors });
+//         }
+//     } catch (error) {
+//         res.status(400).json({ message: error.message });
+//     }
+// };
+
+// exports.updateSetting = async (req, res) => {
+//     try {
+//         // const validatedData = schemaValidator(settingSchema, req.body);
+//         // if (validatedData.success) {
+//         const setting = await Setting.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         if (!setting) {
+//             return res.status(404).json({ message: 'Setting not found' });
+//         }
+
+//         // Respond with success message
+//         res.status(200).json({ data: setting, message: 'Setting updated successfully' });
+//         // } else {
+//         //     res.status(401).json({ message: validatedData.errors });
+//         // }
+//     } catch (error) {
+//         res.status(400).json({ message: error.message });
+//     }
+// };
 
 exports.getAllSettings = async (req, res) => {
     try {
